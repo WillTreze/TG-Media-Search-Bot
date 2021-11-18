a754181fa94c63736a4558e43e5417eb24cba193
Will <48420466+WillTreze@users.noreply.github.com>
2021-11-18 14:27:03 -0300

    removing unnecessary buttons

--- plugins/inline.py
+++ plugins/inline.py
@@ -70,13 +70,13 @@ async def answer(bot, query):
                            switch_pm_parameter="okay")
 
 
-def get_reply_markup(username, query):
-    url = 't.me/share/url?url=' + quote(SHARE_BUTTON_TEXT.format(username=username))
-    buttons = [[
-        InlineKeyboardButton('Search again', switch_inline_query_current_chat=query),
-        InlineKeyboardButton('Share bot', url=url),
-    ]]
-    return InlineKeyboardMarkup(buttons)
+# def get_reply_markup(username, query):
+#     url = 't.me/share/url?url=' + quote(SHARE_BUTTON_TEXT.format(username=username))
+#     buttons = [[
+#         InlineKeyboardButton('Search again', switch_inline_query_current_chat=query),
+#         InlineKeyboardButton('Share bot', url=url),
+#     ]]
+#     return InlineKeyboardMarkup(buttons)
 
 
 def get_size(size):
