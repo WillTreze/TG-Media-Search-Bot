cd79173b2b173d29f6726131889ffd4a15cb1edb
Will <48420466+WillTreze@users.noreply.github.com>
2021-11-19 21:07:58 -0300

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 from utils import save_file
 from info import CHANNELS
 
-media_filter = filters.document | filters.video | filters.audio | filters.image
+media_filter = filters.document | filters.video | filters.audio | filters.media_group
 
 
 @Client.on_message(filters.chat(CHANNELS) & media_filter)
