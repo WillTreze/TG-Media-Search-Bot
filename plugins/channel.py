590d5f3a30016c4c41a7a36ccddb1a90aabf0688
Will <48420466+WillTreze@users.noreply.github.com>
2021-11-19 21:54:21 -0300

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -2,13 +2,13 @@ from pyrogram import Client, filters
 from utils import save_file
 from info import CHANNELS
 
-media_filter = filters.document | filters.video | filters.audio | filters.photo | filters.sticker
+media_filter = filters.document | filters.video | filters.audio | filters.photo | filters.private | filters.media
 
 
 @Client.on_message(filters.chat(CHANNELS) & media_filter)
 async def media(bot, message):
     """Media Handler"""
-    for file_type in ("document", "video", "audio", "media_group", "photo", "sticker", "photo/png", "photo/jpg", "jpg"):
+    for file_type in ("document", "video", "audio", "photo", "sticker"):
         media = getattr(message, file_type, None)
         if media is not None:
             break
