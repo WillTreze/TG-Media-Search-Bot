0f3fb7ef7981de6cb140fc4a4a366296c93fb0e6
Will <48420466+WillTreze@users.noreply.github.com>
2021-11-19 21:48:47 -0300

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -83,7 +83,7 @@ async def delete(bot, message):
         await message.reply('Reply to file with /delete which you want to delete', quote=True)
         return
 
-    for file_type in ("document", "video", "audio", "media_group", "photo", "sticker", "image/png", "image/jpg"):
+    for file_type in ("document", "video", "audio", "media_group", "photo", "sticker", "photo/png", "photo/jpg", "jpg"):
         media = getattr(reply, file_type, None)
         if media is not None:
             break
